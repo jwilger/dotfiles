b78f9750a343427edd3ec35673ecef4cb5728d86
John Wilger <john@johnwilger.com>
2018-02-28 18:03:23 +0000

    Update SSH_AUTH_SOCK in reconnected tmux sessions

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -7,3 +7,23 @@ git_prompt_info() {
 }
 setopt promptsubst
 PS1='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%c%{$reset_color%}$(git_prompt_info) %# '
+
+if [ -n "$TMUX" ]; then
+    function refresh {
+        sshauth=$(tmux show-environment | grep "^SSH_AUTH_SOCK")
+        if [ $sshauth ]; then
+            export $sshauth
+        fi
+        display=$(tmux show-environment | grep "^DISPLAY")
+        if [ $display ]; then
+            export $display
+        fi
+    }
+else
+    function refresh { }
+fi
+
+function preexec {
+    # Refresh environment if inside tmux
+    refresh
+}
