88d7ec898cd04a12f8b2764ccd06f0ad4c0c3825
John Wilger <john@johnwilger.com>
2018-11-02 09:04:34 -0700

    Revert "Adds .config dir"
    This reverts commit 8fe761d4aba54ef273c7d9beb541de89cdd7053a.

--- config/Cerebro/plugins/node_modules/cerebro-open-web/test/index.spec.js
+++
@@ -1,8 +0,0 @@
-const plugin = require('../src/index')
-const { assert } = require('chai')
-
-describe('Plugin', () => {
-  it('contains plugin function', () => {
-    assert(typeof plugin.fn === 'function')
-  })
-})
