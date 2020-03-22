157d937bb75d8954d2db1534342e78feba02efff
John Wilger <john@johnwilger.com>
2020-03-22 11:42:07 -0700

    add coc config dir to dotfiles

---
+++ config/coc/extensions/node_modules/coc-spell-checker/node_modules/cspell-trie-lib/node_modules/gensequence/dist/index.d.ts
@@ -0,0 +1,5 @@
+export * from './GenSequence';
+export { Sequence, LazyIterable, IterableLike, SequenceBuilder } from './types';
+export * from './builder';
+import * as _operators from './operators';
+export declare const operators: typeof _operators;
