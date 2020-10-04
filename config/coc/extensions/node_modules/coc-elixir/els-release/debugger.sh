722ae054d0dd43afd8192ead0ffd859e0c4e448c
John Wilger <john@johnwilger.com>
2020-10-03 17:27:20 -0700

    Lots of updates

--- config/coc/extensions/node_modules/coc-elixir/els-release/debugger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Launches the debugger. This script must be in the same directory as the compiled .ez archives.
-
-dir=$(dirname "$0")
-
-export ELS_MODE=debugger
-export ELS_SCRIPT="ElixirLS.Debugger.CLI.main()"
-
-exec "${dir}/launch.sh"
