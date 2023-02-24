d737ea0e5065442ad8f226c9214c2bd5f36e31a0
John Wilger <jwilger@gregor.valhalla>
2023-02-24 08:39:16 -0800

    adds better defaults to my launch-dc function

--- omz/functions.zsh
+++ omz/functions.zsh
@@ -1,7 +1,8 @@
 launch-dc() {
-  name=$1
-  container=$2
-  container_path=${3-/workspaces}
+  default_name=`basename $PWD`
+  name=${1-${default_name}}
+  container=${2-app}
+  container_path=${3-/workspace}
   export COMPOSE_PROJECT_NAME=$name
   asdf shell nodejs lts
   devcontainer up --workspace-folder . --dotfiles-repository https://github.com/jwilger/dotfiles.git
