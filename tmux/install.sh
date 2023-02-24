d5ce4f4d9b0126bf25533fdd7ca386166ee780d0
John Wilger <jwilger@gregor.valhalla>
2023-02-24 08:40:23 -0800

    No longer install tmux via asdf

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,22 +2,8 @@
 
 sudo apt install -y \
   powerline \
-  fonts-powerline \
-  bison \
-  automake \
-  autotools-dev
-
-. $HOME/.asdf/asdf.sh
-
-asdf plugin add tmux
-asdf install tmux 3.3
-asdf global tmux 3.3
+  fonts-powerline
 
 rm -rf $HOME/.tmux.conf 
-rm -rf $HOME/.tmux
 
 ln -sf $DOTFILES_LOCATION/tmux/tmux.conf $HOME/.tmux.conf
-ln -sf $DOTFILES_LOCATION/tmux/tmux $HOME/.tmux
-
-rm -rf ~/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
