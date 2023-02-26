24cb56008b78c840102dcdff809b59a765c6da77
John Wilger <john@johnwilger.com>
2023-02-26 10:58:53 -0800

    set up rust-tools and trouble vim plugins

--- lvim/config.lua
+++ lvim/config.lua
@@ -167,8 +167,100 @@ lvim.builtin.treesitter.highlight.enable = true
 -- Additional Plugins
 lvim.plugins = {
   { "shaunsingh/nord.nvim" },
-  { "jamestthompson3/nvim-remote-containers" }
+  { "jamestthompson3/nvim-remote-containers" },
+  {
+    "simrat39/rust-tools.nvim",
+    config = function()
+      local status_ok, rust_tools = pcall(require, "rust-tools")
+      if not status_ok then
+        return
+      end
+
+      local opts = {
+        tools = {
+          executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+          reload_workspace_from_cargo_toml = true,
+          inlay_hints = {
+            auto = true,
+            only_current_line = false,
+            show_parameter_hints = true,
+            parameter_hints_prefix = "<-",
+            other_hints_prefix = "=>",
+            max_len_align = false,
+            max_len_align_padding = 1,
+            right_align = false,
+            right_align_padding = 7,
+            highlight = "Comment",
+          },
+          hover_actions = {
+            --border = {
+            --        { "╭", "FloatBorder" },
+            --        { "─", "FloatBorder" },
+            --        { "╮", "FloatBorder" },
+            --        { "│", "FloatBorder" },
+            --        { "╯", "FloatBorder" },
+            --        { "─", "FloatBorder" },
+            --        { "╰", "FloatBorder" },
+            --        { "│", "FloatBorder" },
+            --},
+            auto_focus = true,
+          },
+        },
+        server = {
+          on_attach = require("lvim.lsp").common_on_attach,
+          on_init = require("lvim.lsp").common_on_init,
+          settings = {
+            ["rust-analyzer"] = {
+              checkOnSave = {
+                command = "clippy"
+              }
+            }
+          },
+        },
+      }
+      --local extension_path = vim.fn.expand "~/" .. ".vscode/extensions/vadimcn.vscode-lldb-1.7.3/"
+
+      --local codelldb_path = extension_path .. "adapter/codelldb"
+      --local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+
+      --opts.dap = {
+      --        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+      --}
+      rust_tools.setup(opts)
+    end,
+    ft = { "rust", "rs" },
+  },
+  {
+    "folke/trouble.nvim",
+    requires = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        auto_open = true,
+        auto_close = true
+      }
+    end
+  }
 }
+
+-- Lua
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+  { silent = true, noremap = true }
+)
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
