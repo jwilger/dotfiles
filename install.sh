051e9751e4df458aa658b177c8a6a08e71b5f5a0
John Wilger <john@johnwilger.com>
2023-02-27 07:45:01 -0800

    misc updates

--- install.sh
+++ install.sh
@@ -6,6 +6,24 @@
 DOTFILES_LOCATION=$(pwd)
 export DOTFILES_LOCATION;
 
+export USER=`whoami`
+
+echo "Running dotfiles installation as $USER"
+
+echo "Environment contains:"
+env
+
+echo "Ensuring $USER owns /home/$USER/dotfiles"
+sudo chown -R $USER:$USER /home/$USER/dotfiles
+echo "Ensuring /home/$USER/.config directory exists"
+sudo mkdir -p /home/$USER/.config
+echo "Ensuring $USER owns /home/$USER/.config"
+sudo chown -R $USER:$USER /home/$USER/.config
+echo "Ensuring .tool-versions file exists"
+sudo touch /home/$USER/.tool-versions
+echo "Ensuring $USER owns .tool-versions"
+sudo chown -R $USER:$USER /home/$USER/.tool-versions
+
 ###
 # Install dependencies
 ###
@@ -16,5 +34,4 @@ export DOTFILES_LOCATION;
 ./bin/dotfiles install lvim
 
 asdf reshim
-sudo chown -R $USER:$USER .
 sudo chown -R $USER:$USER ~/.config
