91e3af164bc537cc742fd050c41fa2c308a74b98
John Wilger <john@johnwilger.com>
2023-04-09 08:03:34 -0700

    Default to starting vim with all code unfolded

--- lvim/config.lua
+++ lvim/config.lua
@@ -4,7 +4,7 @@ vim.o.cc = 80
 vim.o.showtabline = 0
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel = 0
+vim.opt.foldlevel = 99
 
 lvim.builtin.bufferline.active = false
 lvim.builtin.breadcrumbs.active = false
