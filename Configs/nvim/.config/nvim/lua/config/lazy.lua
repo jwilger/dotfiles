c3d0240f2276e761c7234cf391ce47f2ed500520
John Wilger <john@johnwilger.com>
2025-04-01 05:57:32 -0700

    Correctly set neovim colorscheme to catppuccin-macchiato

--- Configs/nvim/.config/nvim/lua/config/lazy.lua
+++ Configs/nvim/.config/nvim/lua/config/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "catppuccin" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
@@ -51,3 +51,4 @@ require("lazy").setup({
     },
   },
 })
+vim.cmd.colorscheme("catppuccin-macchiato")
